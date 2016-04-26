2809b501b8f2973319509fba291bb37655ed6fa1
Rossi Oddet <rossi.oddet@gmail.com>
2016-04-26 14:15:17 +0200

    save

--- operateurs-console/src/Titit.java
+++ operateurs-console/src/Titit.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 
 public class Titit {
+	
+	public static void main(String[] args) {
+		System.out.println("ghooo");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nombre");
+		double nb = sc.nextDouble();
+		System.out.println("NB = " + nb);
+		
+		
+		sc.close();
+	}
 
 }
